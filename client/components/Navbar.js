e496706d124b82886fddd5e90b8ba73fa44ff2f4
David <davruiz07@gmail.com>
2022-03-01 18:20:10 -0500

    Product Componenet -Issues with react hooks persist-

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import { Link } from 'react-router-dom'
+import { logout } from '../store'
 
-const Navbar = () =>  {
+const Navbar = () => {
   const isLoggedIn = useSelector(state => !!state.auth.id)
   const dispatch = useDispatch()
 
@@ -15,7 +15,7 @@ const Navbar = () =>  {
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
-            <a href="#" onClick={ () => dispatch(logout()) }>
+            <a href="#" onClick={() => dispatch(logout())}>
               Logout
             </a>
           </div>
@@ -24,12 +24,13 @@ const Navbar = () =>  {
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <Link to="/">Store Page</Link>
           </div>
         )}
       </nav>
       <hr />
     </div>
-    )
+  )
 }
 
 export default Navbar
---
+++ client/components/Products.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchProducts } from '../redux/products'
+
+function Product() {
+    const products = [{ name: 'shirt', price: 2.30, imageUrl: 'https://clipartix.com/wp-content/uploads/2016/05/T-shirt-blank-shirt-clip-art-free-vector-in-open-office-drawing-svg.jpg', desription: 'this is a test' }, { name: 'shirt', price: 2.30, imageUrl: 'https://clipartix.com/wp-content/uploads/2016/05/T-shirt-blank-shirt-clip-art-free-vector-in-open-office-drawing-svg.jpg', desription: 'this is a test' }, { name: 'Shirt', price: 2.32, imageUrl: 'https://clipartix.com/wp-content/uploads/2016/05/T-shirt-blank-shirt-clip-art-free-vector-in-open-office-drawing-svg.jpg', desription: 'this is a test' }, { name: 'Shirt', price: 2.30, imageUrl: 'https://clipartix.com/wp-content/uploads/2016/05/T-shirt-blank-shirt-clip-art-free-vector-in-open-office-drawing-svg.jpg', desription: 'this is a test' }, { name: 'shirt', price: 2.30, imageUrl: 'https://clipartix.com/wp-content/uploads/2016/05/T-shirt-blank-shirt-clip-art-free-vector-in-open-office-drawing-svg.jpg', desription: 'this is a test' }]
+
+    if (products.length === 0) {
+        return (<h1>No Items To Sell!</h1>)
+    } else {
+        return (
+            <div>
+                <h3>Here are the Items</h3>
+                <div className="allProd">
+                    {products.map((current, idx) => {
+                        return (
+                            <div className="singleProduct" key={idx}>
+                                <img src={current.imageUrl} width="190" height="225" />
+                                <div className="productInfo">
+                                    <div>
+                                        <p>{current.name}</p>
+                                        <p>${current.price.toFixed(2)}</p>
+                                    </div>
+                                    <button>Add To Cart</button>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Product
