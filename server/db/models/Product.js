5bb2e0d6189ec891e72a7fe569ea0b3e35786a38
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-01 14:29:38 -0500

    Created products model and initial api routes

---
+++ server/db/models/Product.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+
+  imageUrl: {
+    type: Sequelize.TEXT,
+    validate: {
+      isUrl: true,
+    },
+  },
+
+  description: {
+    type: Sequelize.TEXT,
+  },
+});
+
+module.exports = Product;
