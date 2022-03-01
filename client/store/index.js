282b2a32c17a0c0d2fd5674cf4c80d536a4b6e80
Greg Lee <greglee0921@gmail.com>
2022-03-01 15:51:38 -0500

    Added products reducer and imported into store

--- client/store/index.js
+++ client/store/index.js
@@ -2,13 +2,14 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
+import appReducer from '../redux'
+//import auth from './auth'
 
-const reducer = combineReducers({ auth })
+//const reducer = combineReducers({ auth })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+const store = createStore(appReducer, middleware)
 
 export default store
-export * from './auth'
+//export * from './auth'
