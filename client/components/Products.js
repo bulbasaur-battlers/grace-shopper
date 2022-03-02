8510025882a8f52137e09db68ac02207b6f3bcf6
David <davruiz07@gmail.com>
2022-03-02 17:09:52 -0500

    singlePage base positions

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProducts } from '../store/products';
+import { Link } from 'react-router-dom'
 
 function Product() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, []);
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
 
-  let products = useSelector((state) => state.products);
+    let products = useSelector((state) => state.products);
 
-  if (products.length === 0) {
-    return <h1>No Items To Sell!</h1>;
-  } else {
-    return (
-      <div>
-        <h3>Here are the Items</h3>
-        <div className="allProd">
-          {products.map((current, idx) => {
-            return (
-              <div className="singleProduct" key={idx}>
-                <img src={current.imageUrl} width="190" height="225" />
-                <div className="productInfo">
-                  <div>
-                    <p>{current.name}</p>
-                    <p>${current.price.toFixed(2)}</p>
-                  </div>
-                  <button>Add To Cart</button>
+    if (products.length === 0) {
+        return <h1>No Items To Sell!</h1>;
+    } else {
+        return (
+            <div>
+                <h3>Here are the Items</h3>
+                <div className="allProd">
+                    {products.map((current) => {
+                        return (
+                            <div className="singleProduct" key={current.id}>
+                                <Link to={`/products/${current.id}`}>
+                                    <img src={current.imageUrl} width="190" height="225" />
+                                </Link>
+                                <div className="productInfo">
+                                    <div>
+                                        <p>{current.name}</p>
+                                        <p>${current.price.toFixed(2)}</p>
+                                    </div>
+                                    <button className="button-60">Add To Cart</button>
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default Product;
---
+++ client/components/SingleProduct.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { fetchSingleProduct } from '../store/singleProduct'
+
+
+function SingleProduct() {
+
+    const dispatch = useDispatch()
+    useEffect(() => { dispatch(fetchSingleProduct(params.productId)) }, [])
+
+    let product = useSelector(state => state.singleProduct)
+    const params = useParams()
+
+    if (product.length === 0) {
+        return <h1>Loading</h1>
+    } else {
+        return (
+            <div className="singleItem">
+                <img src={product.imageUrl} width="390" height="425" />
+                <div className="singleProductInfo">
+                    <h2>{product.name}</h2>
+                    <p>${product.price.toFixed(2)}</p>
+                    <button id="SPbutton" className='button-60'>Add To Cart</button>
+                    <p id='descriptor'>{product.description}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SingleProduct
