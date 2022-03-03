f2593a3092036a69343a3a7723c0567509560a92
Dan Lee <dan.jisang.lee@gmail.com>
2022-03-03 18:14:12 -0500

    base skeleton and styling for cart

--- client/components/ViewCart.js
+++ client/components/ViewCart.js
@@ -2,43 +2,110 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchOrder } from '../store/currentOrder';
 import { Link } from 'react-router-dom';
-
+const convertPennies = (pennies) => {
+  if (Math.floor(pennies / 100) === 0) {
+    return pennies % 100 < 10 ? `00.0${pennies % 100}` : `00.${pennies % 100}`;
+  } else {
+    if (pennies % 100 === 0) {
+      return `${Math.floor(pennies / 100)}.00`;
+    } else if (pennies % 100 < 10) {
+      return `${Math.floor(pennies / 100)}.0${pennies % 100}`;
+    } else {
+      return `${Math.floor(pennies / 100)}.${pennies % 100}`;
+    }
+  }
+};
 function ViewCart() {
   const dispatch = useDispatch();
+  let products = [];
+  let total = 0;
 
   useEffect(() => {
     dispatch(fetchOrder());
   }, []);
   let cart = useSelector((state) => state.currentOrder);
-  let products = [];
-  if (cart.products) products = cart.products;
+
+  if (cart.products) {
+    products = cart.products;
+    products.forEach((currProd) => {
+      total += currProd.pennies * currProd.orderproduct.quantity;
+    });
+  }
 
   if (products.length === 0) {
     return <h1>No Items In Cart</h1>;
   } else {
     return (
       <div>
-        <h3>Products in your cart</h3>
-        <div className="allProd">
-          {products.map((current) => {
-            return (
-              <div className="singleProduct" key={current.id}>
-                <Link to={`/products/${current.id}`}>
-                  <img src={current.imageUrl} width="190" height="225" />
-                </Link>
-                <div className="productInfo">
-                  <div>
-                    <p>{current.name}</p>
-                    <p>${current.price}</p>
+        <h3 className="cart-title">Products in your cart</h3>
+        <div className="cart-container">
+          <div className="cart-items-container">
+            {products.map((current) => {
+              return (
+                <div className="cart-item" key={current.id}>
+                  <div className="cart-item-image">
+                    <Link to={`/products/${current.id}`}>
+                      <img src={current.imageUrl} width="190" height="225" />
+                    </Link>
                   </div>
-                  <button className="button-60">Add To Cart</button>
+                  <div className="cart-item-info">
+                    <p>{current.name}</p>
+                    <p>{current.price}</p>
+                  </div>
+                  <div className="cart-item-update">
+                    <div>
+                      <p>
+                        Total:{' '}
+                        {convertPennies(
+                          current.pennies * current.orderproduct.quantity
+                        )}
+                      </p>
+                    </div>
+                    <div>
+                      <p>Quantity: {current.orderproduct.quantity}</p>
+                    </div>
+                    <div>
+                      <button className="button-60">Update Item</button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <div className="cart-total-submit">
+            <div>
+              <p>Cart Total: {convertPennies(total)}</p>
+            </div>
+            <div>
+              <button className="button-60">Checkout</button>
+            </div>
+          </div>
         </div>
       </div>
     );
+    // return (
+    //   <div>
+    //     <h3>Products in your cart</h3>
+    //     <div className="allProd">
+    //       {products.map((current) => {
+    //         return (
+    //           <div className="singleProduct" key={current.id}>
+    //             <Link to={`/products/${current.id}`}>
+    //               <img src={current.imageUrl} width="190" height="225" />
+    //             </Link>
+    //             <div className="productInfo">
+    //               <div>
+    //                 <p>{current.name}</p>
+    //                 <p>${current.price}</p>
+    //               </div>
+    //               <button className="button-60">Add To Cart</button>
+    //             </div>
+    //           </div>
+    //         );
+    //       })}
+    //     </div>
+    //   </div>
+    // );
   }
 }
 
