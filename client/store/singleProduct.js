678ca684e5238706d933eeb77c9bfb815e2e1353
David <davruiz07@gmail.com>
2022-03-03 18:20:07 -0500

    api done, redux store in progress

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 const GET_PRODUCT = 'GET_PRODUCT';
+const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+const DELETE_PRODUCT = 'DELETE_PRODUCT'
 
 export const getProduct = (product) => {
   return {
@@ -8,6 +10,20 @@ export const getProduct = (product) => {
     product,
   };
 };
+
+export const deleteProduct = (product) => {
+  return {
+    type: DELETE_PRODUCT,
+    product
+  }
+}
+
+export const updateProduct = (product) => {
+  return {
+    type: UPDATE_PRODUCT,
+    product
+  }
+}
 
 export const fetchSingleProduct = (id) => {
   return async (dispatch) => {
@@ -20,9 +36,35 @@ export const fetchSingleProduct = (id) => {
   };
 };
 
+export const changeProduct = (product) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`/api/products/${product.id}`, product)
+      dispatch(updateProduct(data))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+export const removeProduct = (product) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.put(`/api/products/${product.id}`, product)
+      dispatch(updateProduct(data))
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
 export default function singleProductReducer(state = [], action) {
   switch (action.type) {
     case GET_PRODUCT:
+      return action.product;
+    case UPDATE_PRODUCT:
+      return action.product;
+    case DELETE_PRODUCT:
       return action.product;
     default:
       return state;
