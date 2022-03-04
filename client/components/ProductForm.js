653e30718c612b350ec007027de02c9c7d378a23
David <davruiz07@gmail.com>
2022-03-04 17:41:17 -0500

    Product form created

---
+++ client/components/ProductForm.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { makeProduct } from '../store/singleProduct'
+
+
+export default const ProductForm = () => {
+    const dispatch = useDispatch
+}
