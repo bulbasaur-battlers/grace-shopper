930e666e388f545b72cebcc7a2cd6faee1ad78cb
David <davruiz07@gmail.com>
2022-03-03 19:03:36 -0500

    singleProductstore Changes

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const TOKEN = 'token'
 const GET_PRODUCT = 'GET_PRODUCT';
 const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
-const DELETE_PRODUCT = 'DELETE_PRODUCT'
+const DELETE_PRODUCT = 'DELETE_PRODUCT';
+const ADD_PRODUCT = 'ADD_PRODUCT'
 
 export const getProduct = (product) => {
   return {
@@ -14,6 +16,13 @@ export const getProduct = (product) => {
 export const deleteProduct = (product) => {
   return {
     type: DELETE_PRODUCT,
+    product
+  }
+}
+
+export const addProduct = (product) => {
+  return {
+    type: ADD_PRODUCT,
     product
   }
 }
@@ -39,8 +48,34 @@ export const fetchSingleProduct = (id) => {
 export const changeProduct = (product) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.put(`/api/products/${product.id}`, product)
-      dispatch(updateProduct(data))
+      const token = window.localStorage.getItem(TOKEN)
+      if (token) {
+        //If anythings broken its here
+        const { data } = await axios.put(`/api/products/${product.id}`, product, {
+          headers: {
+            authorization: token
+          }
+        })
+        dispatch(updateProduct(data))
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+export const makeProduct = (product) => {
+  return async (dispatch) => {
+    try {
+      const token = window.localStorage.getItem(TOKEN)
+      if (token) {
+        const { data } = await axios.post(`/api/products/${product.id}`, product, {
+          headers: {
+            authorization: token
+          }
+        })
+        dispatch(addProduct(data))
+      }
     } catch (err) {
       console.log(err)
     }
@@ -50,8 +85,15 @@ export const changeProduct = (product) => {
 export const removeProduct = (product) => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.put(`/api/products/${product.id}`, product)
-      dispatch(updateProduct(data))
+      const token = window.localStorage.getItem(TOKEN)
+      if (token) {
+        const { data } = await axios.delete(`/api/products/${product.id}`, {
+          headers: {
+            authorization: token
+          }
+        })
+        dispatch(deleteProduct(data))
+      }
     } catch (err) {
       console.log(err)
     }
@@ -62,6 +104,8 @@ export default function singleProductReducer(state = [], action) {
   switch (action.type) {
     case GET_PRODUCT:
       return action.product;
+    case ADD_PRODUCT:
+      return action.product
     case UPDATE_PRODUCT:
       return action.product;
     case DELETE_PRODUCT:
